bbffc36ebf37105415b1414345b82b3acccc62e2
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-24 19:32:28 +0700

    Add logging functionality with Logback configuration and update main method to use logger

--- src/main/java/com/coffeecode/App.java
+++ src/main/java/com/coffeecode/App.java
@@ -1,13 +1,14 @@
 package com.coffeecode;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Just starting apps");
+        System.out.println("Hello World!");
     }
 }
