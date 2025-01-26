a29b97cb0281f1369d1c08906c354d6c565b3deb
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-26 20:39:59 +0700

    Refactor FlowCalculationServiceImpl to improve flow calculation logic and add default handling for pressure input

--- src/main/java/com/coffeecode/logic/flow/FlowCalculationServiceImpl.java
+++ src/main/java/com/coffeecode/logic/flow/FlowCalculationServiceImpl.java
@@ -1,71 +1,77 @@
 package com.coffeecode.logic.flow;
 
-import com.coffeecode.domain.constants.ElevationRange;
+import com.coffeecode.domain.constants.PhysicalConstants;
 import com.coffeecode.domain.constants.SimulationDefaults;
 import com.coffeecode.domain.entity.Pipe;
-import com.coffeecode.domain.objects.Distance;
-import com.coffeecode.domain.objects.PipeProperties;
-import com.coffeecode.domain.objects.Volume;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * FlowCalculationServiceImpl provides the implementation for calculating flow
  * parameters (flow rate, pressure, velocity, head loss) for a pipe network.
  */
+@Slf4j
 @RequiredArgsConstructor
 public class FlowCalculationServiceImpl implements FlowCalculationService {
 
     @Override
     public FlowResult calculateFlow(Pipe pipe, double pressureIn) {
-        // Get pipe properties
-        PipeProperties properties = pipe.getProperties();
-        Distance length = properties.getLength();
-        Volume capacity = properties.getCapacity();
+        double diameter = pipe.getDiameter();
+        double roughness = pipe.getRoughness();
+        double length = pipe.getLength().getMeters();
 
-        // Apply default values for missing parameters
-        double diameter = properties.getDiameter();
-        double roughness = properties.getRoughness();
-        double velocity = SimulationDefaults.FLOW_VELOCITY;
-        double pressure = pressureIn > 0 ? pressureIn : SimulationDefaults.PRESSURE_IN;
-
-        // Ensure the elevation is within the valid range
-        double elevation = SimulationDefaults.ELEVATION;
-        if (elevation < ElevationRange.MIN.getValue() || elevation > ElevationRange.MAX.getValue()) {
-            throw new IllegalArgumentException("Elevation value is out of range.");
+        // Use defaults if pressure is zero
+        if (pressureIn <= 0) {
+            return calculateWithDefaults(pipe);
         }
 
-        // Calculate flow rate, pressure, velocity, and head loss
+        double velocity = calculateVelocity(pressureIn, diameter);
         double flowRate = calculateFlowRate(diameter, velocity);
-        double pressureOut = calculatePressure(pressure, length.getValue(), roughness);
-        double velocityOut = calculateVelocity(flowRate, diameter);
-        double headLoss = calculateHeadLoss(length.getValue(), diameter, roughness, flowRate);
+        double headLoss = calculateHeadLoss(length, diameter, roughness, flowRate);
+        double pressureOut = calculatePressureOut(pressureIn, headLoss);
 
         return FlowResult.builder()
                 .flowRate(flowRate)
                 .pressureOut(pressureOut)
-                .velocityOut(velocityOut)
+                .velocityOut(velocity)
                 .headLoss(headLoss)
                 .build();
     }
 
+    private FlowResult calculateWithDefaults(Pipe pipe) {
+        double velocity = SimulationDefaults.FLOW_VELOCITY;
+        double diameter = pipe.getDiameter();
+        double flowRate = calculateFlowRate(diameter, velocity);
+
+        return FlowResult.builder()
+                .flowRate(flowRate)
+                .pressureOut(PhysicalConstants.ATMOSPHERIC_PRESSURE)
+                .velocityOut(velocity)
+                .headLoss(0.0)
+                .build();
+    }
+
     private double calculateFlowRate(double diameter, double velocity) {
-        // Example calculation (just a simple one for illustration)
         return Math.PI * Math.pow(diameter / 2, 2) * velocity;
     }
 
-    private double calculatePressure(double pressureIn, double length, double roughness) {
-        // Simplified pressure drop calculation (Darcy-Weisbach)
-        return pressureIn - (length * roughness); // This is just an illustrative example
+    private double calculateVelocity(double pressureIn, double diameter) {
+        return Math.sqrt(2 * pressureIn / (PhysicalConstants.WATER_DENSITY));
     }
 
-    private double calculateVelocity(double flowRate, double diameter) {
-        // Simplified velocity calculation
-        return flowRate / (Math.PI * Math.pow(diameter / 2, 2));
+    private double calculateHeadLoss(double length, double diameter,
+            double roughness, double flowRate) {
+        return (8 * roughness * length * Math.pow(flowRate, 2))
+                / (Math.pow(Math.PI, 2) * Math.pow(diameter, 5)
+                * PhysicalConstants.GRAVITY);
     }
 
-    private double calculateHeadLoss(double length, double diameter, double roughness, double flowRate) {
-        // Simplified head loss calculation using Darcy-Weisbach equation
-        return (length * roughness * Math.pow(flowRate, 2)) / Math.pow(diameter, 5);
+    private double calculatePressureOut(double pressureIn, double headLoss) {
+        return Math.max(
+                pressureIn - (headLoss * PhysicalConstants.WATER_DENSITY
+                * PhysicalConstants.GRAVITY),
+                PhysicalConstants.ATMOSPHERIC_PRESSURE
+        );
     }
 }
